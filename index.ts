889a7ff4eee7ce86652e16b506ffd7535f7860df
somyaa <somyasharma0488@gmail.com>
2025-07-17 08:50:08 +0530

    Create index.ts

---
+++ index.ts
@@ -0,0 +1,12 @@
+console.log("Hello");
+let num:number=5;
+console.log(num);
+
+// //func define
+// function sum(a:number, b:number):number {
+//     return a + b;
+// }
+
+
+// //func call
+// console.log(sum(5, 10));
